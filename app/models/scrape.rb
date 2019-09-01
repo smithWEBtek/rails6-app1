a27b0bb6c2ad9d11a3002c2fa3c8832cf797d821
Brad Smith <brad@smithwebtek.com>
2019-09-01 08:17:24 -0400

    WIP 190901-817a

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -12,55 +12,35 @@ class Scrape < ApplicationRecord
       i = Item.new(
         title: d.css('h2 a').text,
         date: d.css('.fl-post-grid-date').text,
-        body: d.css('.fl-post-grid-content p').text,
-        url: d.css('a.fl-post-grid-more').attr('href')
+        url_full_post: d.css('a.fl-post-grid-more').attr('href')
       )
       if i.save
-        self.get_full_data(i)
+        self.get_full_post(i)
       end
     end
   end
 
-  def self.get_full_data(i)
-    doc2 = Nokogiri::HTML(open(i.url))
+  def self.get_full_post(i)
+    doc2 = Nokogiri::HTML(open(i.url_full_post))
     data2 = doc2.css('article')
 
     data2.each do |d|
       i.update(
-        categorized_under: d.css('.post-meta p a').map {|t| t.attributes['rel'].value == 'category tag' ? t.text.gsub('\\', '') : nil }.compact,
-        tagged_as: d.css('.post-meta p a').map {|t| t.attributes['rel'].value == 'tag' ? t.text.gsub('\\', '') : nil }.compact,
-
-
-        d.css('.fl-module-content.fl-node-content')[4].children[1].text
-
-        # "\n\tAmanda Linehan\nCommunications Director, MAPC\n617-933-0705\nalinehan@mapc.org\n"
-
-        # contact_name:
-        # contact_title:
-        # contact_phone:
-        # contact_email:
+        body: d.css('.fl-module.fl-module-rich-text')[1].text.gsub(/\t|\n/, ''),
+        categorized_under: d.css('.post-meta p a').map {|t| t.attributes['rel'].value == 'category tag' ? t.text.gsub(/\\"/, '').split(',') : nil }.compact.flatten,
+        tagged_as: d.css('.post-meta p a').map {|t| t.attributes['rel'].value == 'tag' ? t.text.gsub(/\\"/, '').split(',') : nil }.compact.flatten,
+        contact: d.css('.fl-module-content.fl-node-content')[4].children[1].text.split(/\t|\n/).slice(2),
+        contact_first_name: d.css('.fl-module-content.fl-node-content')[4].children[1].text.split(/\t|\n/).slice(2).split()[0],
+        contact_last_name: d.css('.fl-module-content.fl-node-content')[4].children[1].text.split(/\t|\n/).slice(2).split()[1],
+        contact_title: d.css('.fl-module-content.fl-node-content')[4].children[1].text.split(/\t|\n/).slice(3),
+        contact_phone: d.css('.fl-module-content.fl-node-content')[4].children[1].text.split(/\t|\n/).slice(4),
+        contact_email: d.css('.fl-module-content.fl-node-content')[4].children[1].text.split(/\t|\n/).slice(5),
         author_name: d.css('.byline.author a').children[0].text,
         author_url: d.css('.byline.author a').attr('href').value,
         posted_date_time: d.css('time.updated').attr('datetime').value
         # image_url:
         )
-
-                    # t.string :title
-                    # t.date :date
-                    # t.text :body
-        # t.string :image_url
-                    # t.string :url
-                    # t.text :tagged_as
-                    # t.text :categorized_under
-        # t.string :contact_name
-        # t.string :contact_title
-        # t.string :contact_phone
-        # t.string :contact_email
-                    # t.string :author_name
-                    # t.string :author_url
-                    # t.datetime :posted_date_time
-
-        binding.pry
+        puts '*'
     end
   end
 end
--- db/migrate/20190831214938_create_items.rb
+++ db/migrate/20190831214938_create_items.rb
@@ -4,11 +4,16 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string :title
       t.date :date
       t.text :body
-      t.string :image_url
-      t.string :url
+      t.string :url_full_post
+      t.string :url_image
+      t.string :url_document
+      t.string :url_audio
+      t.string :url_video
       t.text :tagged_as
       t.text :categorized_under
-      t.string :contact_name
+      t.string :contact
+      t.string :contact_first_name
+      t.string :contact_last_name
       t.string :contact_title
       t.string :contact_phone
       t.string :contact_email
