8bf4e6652d614bb3d95275f4a0afd28b0a72aa3d
Brad Smith <brad@smithwebtek.com>
2020-02-03 20:02:26 -0500

    Update model

---
+++ db/migrate/20200204010107_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text  :description
+      t.datetime  :start_date
+      t.datetime  :end_date
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_31_214938) do
+ActiveRecord::Schema.define(version: 2020_02_04_010107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 2019_08_31_214938) do
 
   create_table "scrapes", id: :serial, force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
