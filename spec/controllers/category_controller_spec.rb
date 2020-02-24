31453b04497280910562206dafa54c6b1bdec44b
Brad Smith <brad@smithwebtek.com>
2020-02-23 20:39:23 -0500

    update test, add view

--- spec/controllers/category_controller_spec.rb
+++ spec/controllers/category_controller_spec.rb
@@ -3,29 +3,29 @@ require 'rails_helper'
 RSpec.describe CategoriesController, type: :controller do
 
   login_user
-
+  
   let(:valid_attributes) {
     { :title => "Test title!", :description => "This is a test description"}
   }
-
+  
   let (:valid_session) { {}}
-
+  
   describe "GET #index" do
-    it "returns a success response" do
-      Category.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(response).to be_successful
-    end
-    
-    it "returns a 200 response code" do
-      Category.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(response.code).to eq("200")
-    end
-    
-    it "returns an individual category show page" do
-      category = Category.create! valid_attributes
-      visit "/categories/#{category.id}"
+  it "returns a success response" do
+    Category.create! valid_attributes
+    get :index, params: {}, session: valid_session
+    expect(response).to be_successful
+  end
+  
+  it "returns a 200 response code" do
+    Category.create! valid_attributes
+    get :index, params: {}, session: valid_session
+    expect(response.code).to eq("200")
+  end
+  
+  it "returns an individual category show page" do
+    category = Category.create! valid_attributes
+    visit "/categories/#{category.id}"
       expect(page).to have_content(category.title)
     end
   end
