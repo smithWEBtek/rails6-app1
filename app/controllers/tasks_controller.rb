01684766c964c7a0432f5a883a861a174665bcc1
Brad Smith <brad@smithwebtek.com>
2020-03-02 22:37:32 -0500

    Add methods to Task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,43 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task.delete
+    redirect_to tasks_path
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :description, :start_date, :end_date)
+  end
 end
